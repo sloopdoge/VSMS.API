d2dfc4d6aac687fd12478a21b9604b4d063a8e23
Vladislav Slupko <vslupko@mint-innovations.com>
2025-05-28 14:33:06 +0300

    Initial commit

---
+++ VSMS.Application/Program.cs
@@ -0,0 +1,68 @@
+using Serilog;
+using Serilog.Sinks.Grafana.Loki;
+
+var builder = WebApplication.CreateBuilder(args);
+
+#region Serilog Logger
+
+if (builder.Environment.IsDevelopment())
+{
+    Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Configuration)
+        .CreateLogger();
+}
+
+if (builder.Environment.IsProduction())
+{
+    var lokiUri = builder.Configuration.GetValue<string>("LokiSettings:Url");
+    var appName = builder.Configuration.GetValue<string>("LokiSettings:AppName");
+
+    Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Configuration)
+        .WriteTo.GrafanaLoki(
+            lokiUri,
+            labels: new[]
+            {
+                new LokiLabel
+                {
+                    Key = "app",
+                    Value = appName
+                }
+            },
+            propertiesAsLabels: new[] { "app" })
+        .CreateLogger();
+}
+
+builder.Logging.ClearProviders();
+builder.Host.UseSerilog();
+
+#endregion
+
+Log.Warning("Starting web host");
+try
+{
+    // Add services to the container.
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    builder.Services.AddOpenApi();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.Run();
+}
+catch (Exception e)
+{
+    Log.Fatal(e, e.Message);
+}
+finally
+{
+    Log.Warning("Web host shutdown");
+    Log.CloseAndFlush();
+}
